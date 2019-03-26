19107ea25fda412cceefbfa88af8414ba4436ebd
Chen Meng <94532161@qq.com>
2019-03-26 11:28:06 +0800

    [Master]-Chen Meng Done by Practice 3

--- java-oo-2019-1-8-5-59-45-889-master/src/main/java/practice03/Student.java
+++ java-oo-2019-1-8-5-59-45-889-master/src/main/java/practice03/Student.java
@@ -1,4 +1,42 @@
 package practice03;
 
-public class Student {
+
+public class Student extends Person{
+    private int Klass;
+
+    public Student(String name, int age, int Klass){
+        super(name, age);
+        this.Klass = Klass;
+    }
+    public int getKlass() {
+        return Klass;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "I am a Student. I am at Class " + Klass + ".";
+    }
 }
