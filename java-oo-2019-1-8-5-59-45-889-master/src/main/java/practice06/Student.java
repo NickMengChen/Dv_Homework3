44fd261e66cac967611fcea33a4b29fb91757a94
Chen Meng <94532161@qq.com>
2019-03-26 13:38:24 +0800

    [Master]-Chen Meng Done by Practice 6

--- java-oo-2019-1-8-5-59-45-889-master/src/main/java/practice06/Student.java
+++ java-oo-2019-1-8-5-59-45-889-master/src/main/java/practice06/Student.java
@@ -1,4 +1,40 @@
 package practice06;
+public class Student extends Person{
+    private int Klass;
 
-public class Student {
+    public Student(String name, int age, int Klass){
+        super(name, age);
+        this.Klass = Klass;
+    }
+    public int getKlass() {
+        return Klass;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Student. I am at Class " + Klass + ".";
+    }
 }
--- java-oo-2019-1-8-5-59-45-889-master/src/main/java/practice06/Teacher.java
+++ java-oo-2019-1-8-5-59-45-889-master/src/main/java/practice06/Teacher.java
@@ -1,4 +1,58 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    private int Klass;
+
+    public Teacher(String name, int age, int Klass){
+        super(name, age);
+        this.Klass = Klass;
+    }
+
+    public Teacher (String name, int age){
+        super(name, age);
+        Klass = -1;
+    }
+    public int getKlass() {
+        return Klass;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    public void setKlass(int klass) {
+        Klass = klass;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+        //return super.introduce() + " I am a Student. I am at Class " + Klass + ".";
+//
+//>My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.
+//
+//        如果klass为空，就会返回：
+//
+//>My name is Tom. I am 21 years old. I am a Teacher. I teach No Class.
+
+        if (Klass != -1)
+        {
+            return super.introduce() + " I am a Teacher. I teach Class " + Klass + ".";
+        }
+        return super.introduce() + " I am a Teacher. I teach No Class.";
+    }
 }
