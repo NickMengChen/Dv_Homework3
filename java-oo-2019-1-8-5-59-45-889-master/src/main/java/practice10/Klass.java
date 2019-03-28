0bc7bd1ae83e628d48e4ef72140d87b31b192594
Chen Meng <94532161@qq.com>
2019-03-28 17:08:22 +0800

    [master] Chen Meng: Practice 10 DONE

--- java-oo-2019-1-8-5-59-45-889-master/src/main/java/practice10/Klass.java
+++ java-oo-2019-1-8-5-59-45-889-master/src/main/java/practice10/Klass.java
@@ -1,4 +1,58 @@
 package practice10;
 
+import java.util.ArrayList;
+
 public class Klass {
+    private int Number;
+    private Student leader;
+    private ArrayList<Student> students = new ArrayList<>();
+
+    public ArrayList<Student> getStudents() {
+        return students;
+    }
+
+    public int getNumber() {
+        return Number;
+    }
+
+
+    public boolean appendMember(Student student)
+    {
+        if (students.contains(student))
+            return false;
+        else
+            students.add(student);
+
+        return true;
+    }
+
+    public boolean isLeader(Student student) {
+        if (leader == null)
+
+            return false;
+        return this.leader.getId() == student.getId();
+    }
+
+    public boolean assignLeader(Student leader) {
+
+        if (!students.contains(leader))
+        {
+            System.out.print("It is not one of us.\n");
+            return false;
+        }
+        this.leader = leader;
+        return true;
+    }
+
+    public Student getLeader() {
+        return leader;
+    }
+
+    public Klass(int number) {
+        Number = number;
+    }
+
+    public String getDisplayName(){
+        return "Class " + this.getNumber();
+    }
 }
--- java-oo-2019-1-8-5-59-45-889-master/src/main/java/practice10/Student.java
+++ java-oo-2019-1-8-5-59-45-889-master/src/main/java/practice10/Student.java
@@ -1,4 +1,52 @@
 package practice10;
 
-public class Student {
+public class Student extends Person{
+    private Klass klass;
+
+
+    public Student(int id, String name, int age, Klass klass){
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    @Override
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String introduce() {
+
+        if(klass.isLeader(this)) {
+
+            return super.introduce() + " I am a Student. I am Leader of Class " + klass.getNumber() + ".";
+        }
+
+        System.out.println(klass.getNumber() + "ssss");
+        return super.introduce() + " I am a Student. I am at Class " + klass.getNumber() + ".";
+
+    }
+
+
 }
